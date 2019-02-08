acc2b2dd00a34159071eb172ca20b82e7ef71bec
Jeffrey de Waal <jeffrey.gek@hotmail.com>
2019-02-08 18:18:13 +0100

    First iteration K-nn

--- week1/opdracht3.1/main.py
+++ week1/opdracht3.1/main.py
@@ -23,20 +23,36 @@ def generateLabels(dates):
     return labels
 
 
+
 data = loadCsvToNumpy('dataset1.csv')
+# print(data)
 dates = np.genfromtxt("dataset1.csv", delimiter=";", usecols=[0])
 labels = generateLabels(dates)
 
+# print(labels)
 validationData = loadCsvToNumpy('validation1.csv')
 
 validationDates = np.genfromtxt("validation1.csv", delimiter=";", usecols=[0])
 validationLabels = generateLabels(validationDates)
 
+testpoint = [10,20,30,30,30,30,30]
 
-for i in range(0, len(dates)-1):
-    distance = np.zeros(7)
-    for j in range(0, len(dates)-1):
-        delta = data[i] - data[j]
-        if delta.all() < distance.all():
-            distance = delta
-    print(distance)
+delta = []
+k = 3
+for i in range(0, len(dates)):
+    delta.append(distance.euclidean(testpoint, data[i]))
+
+nearest = [-1, -1, -1]
+nearestindex = [0, 0, 0]
+for i in range(0, len(delta)):
+    for j in range(0, k):
+        if delta[i] < nearest[j] or nearest[j] == -1:
+            nearest[j] = delta[i]
+            nearestindex[j] = i
+            # print(labels[i])
+            break
+print(nearest)
+print(nearestindex)
+for i in range(0, len(nearestindex)):
+    print(labels[nearestindex[i]])
+
