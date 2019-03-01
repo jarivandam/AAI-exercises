69f58f1fa8aff5f6af40edea375afc64f503cf91
jeffrey de Waal <jeffrey.gel@hotmail.com>
2019-03-01 18:16:25 +0100

    Added the start of backpropagation

--- week3/opdracht4.3/neuron.py
+++ week3/opdracht4.3/neuron.py
@@ -4,11 +4,14 @@ LearnRate = 0.1
 
 
 class Neuron():
-    def __init__(self, inputs, weights, threshold):
+    def __init__(self, inputs, weights, threshold, outputs = [], outputWeights = [], hidden = False):
         self.inputs = inputs
         self.weights = weights
         self.output = None
+        self.outputs = outputs
+        self.outputWeights = outputWeights
         self.threshold = threshold
+        self.hidden = hidden
 
     def calc(self):
         result = 0
@@ -28,9 +31,33 @@ class Neuron():
         for i in range(len(self.inputs)):
             result += self.inputs[i] * self.weights[i]
 
+        # for i in range(len(self.inputs)):
+        #     self.weights[i] = (self.weights[i]+LearnRate*self.inputs[i]*(1-tanh(tanh(result)))
+        # 
+        #                        * (desiredActivation-tanh(result)))
+        newWeights = []
         for i in range(len(self.inputs)):
-            self.weights[i] = (self.weights[i]+LearnRate*self.inputs[i]*(1-tanh(tanh(result)))
-                               * (desiredActivation-tanh(result)))
-
+            newWeights.append(self.weights[i]+LearnRate*self.inputs[i]*self.errorCalc(desiredActivation))
+        self.weights = newWeights
         print(self.inputs)
         print(self.weights)
+
+    def errorCalc(self, desiredActivation):
+        result = 0
+        if self.hidden:
+            for i in range(len(self.inputs)):
+                result += self.inputs[i] * self.weights[i]
+            for i in range(len(self.outputs)):
+                weightedError += self.outputWeights[i]* self.outputs[i].errorCalc(desiredActivation)
+            error = (1-tanh(tanh(result)))*weightedError
+            return error
+
+            
+        
+        else:
+            for i in range(len(self.inputs)):
+                result += self.inputs[i] * self.weights[i]
+            error = (1-tanh(tanh(result)))*(desiredActivation-tanh(result))
+            return error
+    
+
